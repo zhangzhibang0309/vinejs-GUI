934e490d8763972163078e25214185dea5ed2bb3
Shawn Zhou <shawnzhou101@163.com>
2022-05-02 18:48:00 +0800

    feat: extensions

---
+++ src/components/ExtSideItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <a
+    class="w-full font-thin uppercase text-gray-500 flex items-center p-4 my-2 transition-colors duration-200 justify-start border-r-4 "
+    href="#" :class="{ 'bg-green-50': id === active, 'border-green-500': id === active, 'text-green-500': id === active }"
+  >
+    <i class="text-left icon orb-core" :class="icon"></i>
+    <span class="mx-4 text-sm font-normal"> {{ name }} </span>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: ['name', 'id', 'icon', 'active'],
+  setup(props, ctx) {
+    return {
+      name: props.name,
+      id: props.id,
+      icon: props.icon
+    }
+  },
+})
+</script>
+
+
+<style lang="scss" scoped>
+  .orb-core {
+    background: url('/orb-core.svg');
+    background-size: contain;
+  }
+
+  .orb-build {
+    background: url('/orb-build.svg');
+    background-size: contain;
+  }
+
+  .orb-custom {
+    background: url('/orb-custom.svg');
+    background-size: contain;
+  }
+
+  .orb-data {
+    background: url('/orb-data.svg');
+    background-size: contain;
+  }
+
+  .orb-deployer {
+    background: url('/orb-deployer.svg');
+    background-size: contain;
+  }
+
+  .orb-feature {
+    background: url('/orb-feature.svg');
+    background-size: contain;
+  }
+
+  .icon {
+    width: 1rem;
+    height: 1rem;
+  }
+</style>
--- src/views/Extensions.vue
+++ src/views/Extensions.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    extensions
+  <div class="flex flex-row container center">
+    <div class="h-auto hidden lg:block my-4 ml-4 shadow-lg relative w-80">
+      <div class="bg-white h-full rounded-2xl dark:bg-gray-700">
+        <div class="flex items-center justify-center pt-6">
+          <img src="/favicon.png" class="ext-logo"/>
+          <span class="text-gray-700 font-normal text-lg">Vine.js 组件市场</span> 
+        </div>
+        <nav class="mt-6">
+          <div>
+            <ExtSideItem v-for="item in extList" :key="item.id"
+              :name="item.name" :id="item.id" :icon="item.icon" :active="active"
+            ></ExtSideItem>
+          </div>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import ExtSideItem from '@/components/ExtSideItem.vue';
+
+const active = 0;
+
+const extList = [
+  {
+    name: '主核心',
+    id: 0,
+    icon: 'orb-core'
+  },
+  {
+    name: '部署器',
+    id: 1,
+    icon: 'orb-deployer'
+  },
+  {
+    name: '扩展链：特征',
+    id: 2,
+    icon: 'orb-feature'
+  },
+  {
+    name: '扩展链：数据',
+    id: 3,
+    icon: 'orb-data'
+  },
+  {
+    name: '扩展链：构建',
+    id: 4,
+    icon: 'orb-build'
+  },
+  {
+    name: '扩展链：样式',
+    id: 5,
+    icon: 'orb-style'
+  },
+  {
+    name: '扩展链：自定义',
+    id: 6,
+    icon: 'orb-custom'
+  },
+]
 
 </script>
 
 <style lang="scss" scoped>
+  .center {
+    margin: 0 auto;
+  }
+
+  .ext-logo {
+    max-width: 3rem;
+    margin-right: 1rem;
+  }
 
 </style>
