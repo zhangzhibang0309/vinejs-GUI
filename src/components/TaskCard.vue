bc8c6cd5d6c3f0e7160eeb8bd80d7ab987f95be8
Shawn Zhou <shawnzhou101@163.com>
2022-05-03 22:19:18 +0800

    feat: task

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="shadow-lg rounded-2xl bg-white dark:bg-gray-800 p-4 mb-8 flex-1 flex items-center">
+    <div class="flex-row gap-4 flex w-full justify-between">
+      <div class="flex task-icon">
+        <i :class="data.icon"></i>
+      </div>
+      <div class="flex flex-col flex-1">
+        <span class="text-gray-600 dark:text-white text-lg font-medium">
+          {{ data.name }}
+        </span>
+        <span class="text-gray-400 text-xs"> {{ data.description }} </span>
+      </div>
+      <button type="button" class="py-2 px-4 bg-green-600 hover:bg-green-700 focus:ring-green-500 
+      focus:ring-offset-green-200 text-white transition ease-in duration-200 text-center text-base font-normal shadow-md focus:outline-none focus:ring-2 
+      focus:ring-offset-2 rounded-lg" @click="runTask(data.name)">
+        运行
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRef } from "vue";
+
+export default defineComponent({
+  props: ['data'],
+  setup(props, ctx) {
+    const data = toRef(props, 'data');
+    const runTask = (command) => {
+
+    }
+    return {
+      data,
+      runTask
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+
+  .task-icon {
+    width: 4rem;
+    & > .task-build {
+      width: 2rem;
+      height: 2rem;
+      background: url('/block.svg');
+      background-size: contain;
+    }
+    & > .task-debug {
+      width: 2rem;
+      height: 2rem;
+      background: url('/bug.svg');
+      background-size: contain;
+    }
+    & > .task-publish {
+      width: 2rem;
+      height: 2rem;
+      background: url('/cloud-server.svg');
+      background-size: contain;
+    }
+    & > .task-clear {
+      width: 2rem;
+      height: 2rem;
+      background: url('/delete.svg');
+      background-size: contain;
+    }
+  }
+</style>
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>
-    task
+  <div class="task-main container flex flex-nowrap flex-col center items-center">
+    <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:justify-between lg:py-16 lg:px-8 flex flex-col title">
+      <h2 class="text-3xl font-normal leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 ">
+        图形化命令面板
+        <br />
+        <span class="text-green-600 text-2xl letter-1">选择一项命令以启动 Vine.js</span>
+      </h2>
+    </div>
+    <div class="w-1/2 flex-1 flex flex-col flex-nowrap">
+      <task-card v-for="(item, index) in TaskList" :key="index" :data="item"></task-card>
+    </div>
   </div>
 </template>
 
 <script setup>
+import TaskCard from '@/components/TaskCard.vue';
+
+const TaskList = [
+  {
+    icon: 'task-build',
+    name: 'list',
+    description: 'Vine.js 将静态页面打包生成至项目根目录下的 dist 文件夹。'
+  },
+  {
+    icon: 'task-debug',
+    name: 'debug',
+    description: '启动一个本地 Node.js 服务器预览页面，如果之前还没有生成页面，Vine.js 将在执行预览前自动生成。'
+  },
+  {
+    icon: 'task-publish',
+    name: 'publish',
+    description: '启动部署器，将静态页面打包上传至服务器，稍后即可发布此网站。'
+  },
+  {
+    icon: 'task-clear',
+    name: 'clear',
+    description: '清除项目目录下的构建缓存。'
+  }
+]
 
 </script>
 
 <style lang="scss" scoped>
+  .center {
+    margin: 0 auto;
+    min-height: 865px;
+  }
 
+  .title {
+    margin: 0;
+  }
 </style>
