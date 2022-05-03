c09815a7680b199c94ca40b5f1744d10ab197976
Shawn Zhou <shawnzhou101@163.com>
2022-05-03 14:08:39 +0800

    feat: orbmentg core animate

--- src/components/Orbment/OrbmentBase.vue
+++ src/components/Orbment/OrbmentBase.vue
@@ -1,6 +1,11 @@
 <template>
   <div class=" orbment-base rounded-lg relative">
-    <div class="flex absolute orbment-left-part">
+    <div class="absolute orbment-left-part">
+      <div class="orbment-core-box">
+        <div class="orbment-core-outer"></div>
+        <div class="orbment-core-middle"></div>
+        <div class="orbment-core-inner"></div>
+      </div>
     </div>
     <div class="flex p-6 orbment-right-part">
       <div class="flex flex-wrap">
@@ -13,6 +18,23 @@
 <script setup></script>
 
 <style lang="scss" scoped>
+
+  @keyframes clockwise {
+    0% { transform: rotate(0deg) }
+    25% { transform: rotate(90deg) }
+    50% { transform: rotate(180deg) }
+    75% { transform: rotate(270deg) }
+    100% { transform: rotate(360deg) }
+  }
+
+  @keyframes counterclockwise {
+    0% { transform: rotate(360deg) }
+    25% { transform: rotate(270deg) }
+    50% { transform: rotate(180deg) }
+    75% { transform: rotate(90deg) }
+    100% { transform: rotate(0deg) }
+  }
+
   .orbment-base {
     display: flex;
     align-items: center;
@@ -26,7 +48,7 @@
     background: url('/orbment-rbg.jpg');
     background-size: cover;
     flex: 1;
-    height: 550px;
+    height: 700px;
     border-radius: .5rem;
     background-repeat: no-repeat;
     box-shadow: #3b3b3ba8 4px 3px 5px 2px;
@@ -37,11 +59,47 @@
     background: url('/orbment-lbg.jpg');
     border-radius: .5rem;
     width: 28rem;
-    background-position: -14rem -7rem;
+    background-position: -27rem -1rem;
     left: -10rem;
     background-size: inherit;
     background-repeat: no-repeat;
     box-shadow: #3b3b3ba8 4px 3px 5px 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .orbment-core-box {
+    width: 15rem;
+    height: 15rem;
+    // border: 1px solid red;
+    margin-bottom: 5rem;
+    position: relative;
   }
 
+  .orbment-core-outer {
+    width: 100%;
+    height: 100%;
+    background: url('/outer.png');
+    background-size: cover;
+    position: absolute;
+    animation: counterclockwise 15s linear .1s infinite running;
+  }
+
+  .orbment-core-middle {
+    width: 100%;
+    height: 100%;
+    background: url('/middle.png');
+    background-size: cover;
+    position: absolute;
+    animation: clockwise 14.5s linear .1s infinite running;
+  }
+
+  .orbment-core-inner {
+    width: 100%;
+    height: 100%;
+    background: url('/inner.png');
+    background-size: cover;
+    position: absolute;
+    animation: counterclockwise 14s linear .1s infinite running;
+  }
 </style>
